cb8ac9f6486c3675dea2cd8a140f57b8a79e0d0f
Svetlana Morozova <svetlanochka@yandex.ru>
2023-07-07 13:01:01 +0300

    start commit

---
+++ src/test/java/org/ru/skypro/coursework/AuctionSystem/AuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ru.skypro.coursework.AuctionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
